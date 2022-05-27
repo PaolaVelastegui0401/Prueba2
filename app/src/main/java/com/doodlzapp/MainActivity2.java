779f0c96b80e0c5e6e2b1fc22dda04e474b1b86c
VelasteguiPaola <paovelastegui@hotmail.com>
2022-05-26 19:06:17 -0500

    Primer Commit

---
+++ app/src/main/java/com/doodlzapp/MainActivity2.java
@@ -0,0 +1,42 @@
+package com.doodlzapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity2 extends AppCompatActivity {
+    private EditText usuario;
+    private EditText contraseña;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        usuario = findViewById(R.id.txt_Usuario);
+        contraseña = findViewById(R.id.txt_Contraseña);
+
+    }
+
+    public void Ingresar(View view) {
+
+        String usu = usuario.getText().toString();
+        String contra = contraseña.getText().toString();
+
+        if(usu.equals("paola")&& contra.equals("paola")){
+
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+
+        }else if(usu.equals("jessica")&& contra.equals("jessica")){
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }else{
+
+        }
+    }
+}
